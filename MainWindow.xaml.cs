147f80c9487c7c83d70119c9af7c5c3b94a71b33
AT K <kabakciatalay@gmail.com>
2024-02-04 13:14:35 +0300

    ui elements placed

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -25,21 +25,26 @@ namespace passgen
             InitializeComponent();
         }
 
+        PasswordCharacters characters = new();
+        Random random = new Random();
+
+
+
         public class PasswordCharacters
         {
-            public string[] UpperChars = { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
-            public string[] LowerChars = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
-            public string[] Numbers = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
-            public string[] Symbols = { "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "=", "{", "}", "[", "]", "|", ":", ";", "\"", "'", "<", ">", "?", "/" };
+            public readonly string[] UpperChars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+            public readonly string[] LowerChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+            public readonly string[] Numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+            public readonly string[] Symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "=", "{", "}", "[", "]", "|", ":", ";", "\"", "'", "<", ">", "?", "/"];
         }
 
-        public class Generate
+        public string Generated()
         {
-            
+            return "";
         }
 
-
-
-
+        private void btnGenerate_Click(object sender, RoutedEventArgs e)
+        {
+        }
     }
 }
