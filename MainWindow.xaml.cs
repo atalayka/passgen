31a3c98f430823d7982750f52015dc78affff51a
AT K <kabakciatalay@gmail.com>
2024-02-11 15:24:04 +0300

    code behind structure; logic

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq;
 using System.Windows;
 using System.Collections.Generic;
 
@@ -7,15 +6,13 @@ namespace passgen;
 public partial class MainWindow : Window
 {
     public static int passwordLength = 0;
+    public static List<string> crudePassword = [];
     public static List<string> password = [];
-    public static List<string> result = [];
 
     public MainWindow()
     {
         InitializeComponent();
-
         passwordLength = (int)CharCount.Value;
-        password = new List<string>();
     }
 
     //operation
@@ -26,66 +23,61 @@ public partial class MainWindow : Window
 
     public class GenerateLowerCase : IGenerate
     {
-        private Random random = new Random();
-        private readonly string[] LowerChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+        private Random random = new Random((int)DateTime.Now.Ticks + 123);
+        private List<string> LowerChars = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
         public void GeneratePassword()
         {
             for (int i = 0; i < passwordLength; i++)
             {
-                int randomIndex = random.Next(LowerChars.Length - 1);
-                password.Add(LowerChars[randomIndex]);
+                int randomIndex = random.Next(LowerChars.Count - 1);
+                crudePassword.Add(LowerChars[randomIndex]);
+                LowerChars.Remove(LowerChars[randomIndex]);
             }
         }
     }
 
     public class GenerateUpperCase : IGenerate
     {
-        private Random random = new Random();
-        private readonly string[] UpperChars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+        private Random random = new Random((int)DateTime.Now.Ticks + 234);
+        private List<string> UpperChars = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
         public void GeneratePassword()
         {
             for (int i = 0; i < passwordLength; i++)
             {
-                int randomIndex = random.Next(UpperChars.Length - 1);
-                password.Add(UpperChars[randomIndex]);
+                int randomIndex = random.Next(UpperChars.Count - 1);
+                crudePassword.Add(UpperChars[randomIndex]);
+                UpperChars.Remove(UpperChars[randomIndex]);
             }
         }
     }
 
     public class GenerateNumbers : IGenerate
     {
-        private Random random = new Random();
-        private readonly string[] Numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-
+        private Random random = new Random((int)DateTime.Now.Ticks + 345);
+        private List<string> Numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
         public void GeneratePassword()
         {
-            for (int i = 0; i < passwordLength; i++)
+            for (int i = 0; i < 10; i++)
             {
-                int randomIndex = random.Next(Numbers.Length - 1);
-                password.Add(Numbers[randomIndex]);
+                int randomIndex = random.Next(Numbers.Count - 1);
+                crudePassword.Add(Numbers[randomIndex]);
+                Numbers.Remove(Numbers[randomIndex]);
             }
         }
     }
 
     public class GenerateSymbols : IGenerate
     {
-        private Random random = new Random();
-        private readonly string[] Symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "=", "{", "}", "[", "]", "|", ":", ";", "\"", "'", "<", ">", "?", "/"];
+        private Random random = new Random((int)DateTime.Now.Ticks + 456);
+        private List<string> Symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "+", "=", "{", "}", "[", "]", "|", ":", ";", "\"", "'", "<", ">", "?", "/"];
         public void GeneratePassword()
         {
             for (int i = 0; i < passwordLength; i++)
             {
-                int randomIndex = random.Next(Symbols.Length - 1);
-                password.Add(Symbols[randomIndex]);
+                int randomIndex = random.Next(Symbols.Count - 1);
+                crudePassword.Add(Symbols[randomIndex]);
+                Symbols.Remove(Symbols[randomIndex]);
             }
-        }
-    }
-
-    public class Shuffle(List<string> password)
-    {
-        public List<string> ShuffleList(List<string> password)
-        {
-            return new List<string>(password.OrderBy(x => Guid.NewGuid()));
         }
     }
 
@@ -116,20 +108,18 @@ public partial class MainWindow : Window
                 GenerateSymbols generateSymbols = new GenerateSymbols();
                 generateSymbols.GeneratePassword();
             }
-            Shuffle shuffle = new Shuffle(password);
-            password = shuffle.ShuffleList(password);
 
-            Random random = new Random();
+            Random random = new Random((int)DateTime.Now.Ticks + 567);
             for (int i = 0; i < passwordLength; i++)
             {
-                int randomIndex = random.Next(password.Count - 1);
-                result.Add(password[randomIndex]);
+                int randomIndex = random.Next(crudePassword.Count - 1);
+                password.Add(crudePassword[randomIndex]);
+                crudePassword.Remove(crudePassword[randomIndex]);
             }
-
-            txtResult.Text = string.Join("", result.ToArray());
+            txtResult.Text = string.Join("", password.ToArray());
             counter++;
             password.Clear();
-            result.Clear();
+            crudePassword.Clear();
         }
 
         else
@@ -155,20 +145,18 @@ public partial class MainWindow : Window
                 GenerateSymbols generateSymbols = new GenerateSymbols();
                 generateSymbols.GeneratePassword();
             }
-            Shuffle shuffle = new Shuffle(password);
-            password = shuffle.ShuffleList(password);
 
-            Random random = new Random();
+            Random random = new Random((int)DateTime.Now.Ticks + 678);
             for (int i = 0; i < passwordLength; i++)
             {
-                int randomIndex = random.Next(password.Count - 1);
-                result.Add(password[randomIndex]);
+                int randomIndex = random.Next(crudePassword.Count - 1);
+                password.Add(crudePassword[randomIndex]);
+                crudePassword.Remove(crudePassword[randomIndex]);
             }
-
-            txtResult.Text = string.Join("", result.ToArray());
+            txtResult.Text = string.Join("", password.ToArray());
             counter++;
             password.Clear();
-            result.Clear();
+            crudePassword.Clear();
         }
     }
 }
